d3cebcbc717d6deee9dfa4132ce7a11d1b243b72
Sergio G <bearwebua@gmail.com>
2020-10-31 01:31:50 -0700

    updates

--- dir_walk_sorted.py
+++ dir_walk_sorted.py
@@ -1,11 +1,35 @@
 import os, sys
 
 def dir_walk_sorted(path:str)->None:
+    """
+    Prints sorted (ASC) list of all files under the same directory
+
+    Example:
+        /home/bearwebua/projects/a/api || .dockerignore || 22 B
+        /home/bearwebua/projects/a/api || requirements.txt || 60 B
+        /home/bearwebua/projects/a/api || Dockerfile - Copy || 341 B
+        /home/bearwebua/projects/a/api || Dockerfile || 341 B
+        /home/bearwebua/projects/a/api || .gitignore || 2034 B
+        /home/bearwebua/projects/a/api || index.py || 5375 B
+        /home/bearwebua/projects/a/api/src || logger.py || 285 B
+        /home/bearwebua/projects/a/api/src || utils.py || 1891 B
+        /home/bearwebua/projects/a/api/src || exceptions.py || 1923 B
+        /home/bearwebua/projects/a/api/src || response.py || 2680 B
+        /home/bearwebua/projects/a/api/src/__pycache__ || logger.cpython-38.pyc || 399 B
+        /home/bearwebua/projects/a/api/src/__pycache__ || utils.cpython-38.pyc || 2029 B
+        /home/bearwebua/projects/a/api/src/__pycache__ || exceptions.cpython-38.pyc || 2151 B
+        /home/bearwebua/projects/a/api/src/__pycache__ || response.cpython-38.pyc || 3340 B
+        /home/bearwebua/projects/a/api/__pycache__ || index.cpython-38.pyc || 5407 B
+        /home/bearwebua/projects/a/api/tests || test_one.py || 2197 B
+        /home/bearwebua/projects/a/api/config || production.py || 710 B
+        /home/bearwebua/projects/a/api/config || development.py || 716 B
+        /home/bearwebua/projects/a/api/config || db_production.db || 8192 B
+    """
 
     if not type(path) is str or not path:
         return ''
     
-    for root, dirs, files in os.walk(path): # recurisve
+    for root, dirs, files in os.walk(path): # recurisve. O(n)
         # sort only in sub dirs
         files = sorted(files, key = lambda x: os.stat(os.path.join(root, x)).st_size) # O(n*log(n))
         for file in files: # O(n)
--- dir_walk_sorted_all.py
+++ dir_walk_sorted_all.py
@@ -3,8 +3,8 @@ from sortedcontainers import SortedDict
 
 def dir_walk_sorted_all(path:str):
     """
-    Prints sorted list of files under the directory (ASC order)
-
+    Prints sorted (ASC) list of all files under the directory including the sub directories
+    
     SortedDict. Keeps our dict/hasmap sorted by key. 
     Values are list of tuples. List have length >1 if multiple files have the same size
        BYTES        PATH          NAME
@@ -14,6 +14,26 @@ def dir_walk_sorted_all(path:str):
         523: [('/home/project/', 'file2'), ('/home/project/', 'file4')]  # same size
      }
 
+     Example:
+        /home/bearwebua/projects/a/api || .dockerignore || 22 B
+        /home/bearwebua/projects/a/api || requirements.txt || 60 B
+        /home/bearwebua/projects/a/api/src || logger.py || 285 B
+        /home/bearwebua/projects/a/api || Dockerfile - Copy || 341 B
+        /home/bearwebua/projects/a/api || Dockerfile || 341 B
+        /home/bearwebua/projects/a/api/src/__pycache__ || logger.cpython-38.pyc || 399 B
+        /home/bearwebua/projects/a/api/config || production.py || 710 B
+        /home/bearwebua/projects/a/api/config || development.py || 716 B
+        /home/bearwebua/projects/a/api/src || utils.py || 1891 B
+        /home/bearwebua/projects/a/api/src || exceptions.py || 1923 B
+        /home/bearwebua/projects/a/api/src/__pycache__ || utils.cpython-38.pyc || 2029 B
+        /home/bearwebua/projects/a/api || .gitignore || 2034 B
+        /home/bearwebua/projects/a/api/src/__pycache__ || exceptions.cpython-38.pyc || 2151 B
+        /home/bearwebua/projects/a/api/tests || test_one.py || 2197 B
+        /home/bearwebua/projects/a/api/src || response.py || 2680 B
+        /home/bearwebua/projects/a/api/src/__pycache__ || response.cpython-38.pyc || 3340 B
+        /home/bearwebua/projects/a/api || index.py || 5375 B
+        /home/bearwebua/projects/a/api/__pycache__ || index.cpython-38.pyc || 5407 B
+        /home/bearwebua/projects/a/api/config || db_production.db || 8192 B
     """
 
     sorted_dict = SortedDict({})
